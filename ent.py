c5283848e54821769de2d6b95b30d1892e9cd24a
Roland Smith <rsmith@xs4all.nl>
2015-05-31 01:04:06 +0200

    Added scc calculation. Re-implemented poz.

--- ent.py
+++ ent.py
@@ -1,17 +1,63 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Author: R.F. Smith <rsmith@xs4all.nl>
-# Last modified: 2015-05-28 00:27:21 +0200
+# Last modified: 2015-05-31 01:04:17 +0200
 #
 # To the extent possible under law, Roland Smith has waived all
 # copyright and related or neighboring rights to the original works in
 # chisquare.py. This work is published from the Netherlands. See
 # http://creativecommons.org/publicdomain/zero/1.0/
 
+"""
+Partial implementation of the ‘ent’ program by John "Random" Walker in Python
+using the numerical Python extension.
+
+See http://www.fourmilab.ch/random/ for the original.
+"""
+
 from os.path import getsize
 import math
 import sys
 import numpy as np
+
+
+def main(args=None):
+    """
+    Calculates and prints figures about the randomness of the input files.
+
+    Arguments:
+        args: List of input files.
+    """
+    for fname in args:
+        data, cnts = readdata(fname)
+        print('File "{}"'.format(fname))
+        e = entropy(cnts)
+        print('- Entropy is {:.6f} bits per byte.'.format(e))
+        outs = '- Arithmetic mean value of data bytes is {:.4f}'
+        print(outs.format(data.mean()), '(random = 127.5).')
+        c = pearsonchisquare(data, cnts)
+        p = pochisq(c)
+        outs = '- χ² distribution for {} samples is {:.2f}, and randomly'
+        print(outs.format(len(data), c))
+        outs = '  would exceed this value {:.2f} percent of the times.'
+        print(outs.format(p*100))
+        d = math.fabs(p*100-50)
+        print("  According to the χ² test, this sequence", end=' ')
+        if d > 49:
+            print("is almost certainly not random")
+        elif d > 45:
+            print("is suspected of being not random.")
+        elif d > 40:
+            print("is close to random, but not perfect.")
+        else:
+            print("looks random.")
+        try:
+            print("- Serial correlation coefficient is ", end="")
+            scc = correlation(data)
+            es = "{:.6f} (totally uncorrelated = 0.0)"
+            print(es.format(scc))
+        except ValueError:
+            print("undefined.")
 
 
 def readdata(name):
@@ -30,9 +76,26 @@ def readdata(name):
     return data, cnts
 
 
+def entropy(counts):
+    """
+    Calculate the entropy of the data represented by the counts array. Returns
+    the entropy in bits per byte.
+
+    Arguments:
+        counts: numpy array of counts for all byte values.
+    """
+    ent = 0.0
+    counts = np.trim_zeros(np.sort(counts))
+    sz = sum(counts)
+    p = counts/sz
+    ent = -sum(p * np.log(p)/math.log(256))
+    return ent*8
+
+
 def pearsonchisquare(d, counts):
-    """Calculate Pearson's χ² (chi square) test for an array of bytes.
-    See [http://en.wikipedia.org/wiki/Pearson%27s_chi-squared_test
+    """
+    Calculate Pearson's χ² (chi square) test for an array of bytes. See
+    [http://en.wikipedia.org/wiki/Pearson%27s_chi-squared_test
     #Discrete_uniform_distribution]
 
     Arguments:
@@ -46,83 +109,71 @@ def pearsonchisquare(d, counts):
     return sum((counts - np)**2/np)
 
 
-def entropy(counts):
-    """Calculate the entropy of the data represented by the counts
-    array. Returns the entropy in bits per byte.
+def correlation(d):
+    """
+    Calculate the serial correlation coefficient of the data.
 
     Arguments:
-        counts: numpy array of counts for all byte values.
+        d: numpy array of unsigned byte values.
+
+    Returns:
+        Serial correlation coeffiecient.
     """
-    ent = 0.0
-    counts = np.trim_zeros(np.sort(counts))
-    sz = sum(counts)
-    p = counts/sz
-    ent = -sum(p * np.log(p)/math.log(256))
-    return ent*8
+    totalc = len(d)
+    a = np.array(d, np.float32)
+    b = np.append(a[1:], a[0])
+    scct1 = np.sum(a*b)
+    scct2 = np.sum(a)**2
+    scct3 = np.sum(a*a)
+    scc = totalc * scct3 - scct2
+    if scc == 0:
+        raise ValueError
+    scc = (totalc * scct1 - scct2) / scc
+    return scc
 
-# _poz() and pochisq() are ported from chisq.c from random.zip;
-# http://www.fourmilab.ch/random/ (also in the public domain)
 
-
-def _poz(z):
-    """Probability of normal z value. Returns cumulative probability
-    from -∞ to z. Adapted from a polynomial approximation in:
-    "Ibbetson D, Algorithm 209, Collected Algorithms of the CACM 1963
-    p. 616" Note: This routine has six digit accuracy, so it is only
-    useful for absolute z values < 6.  For z values >= to 6.0, poz()
-    returns 0.0.
+def poz(z):
+    """
+    Probability of normal z value.
+    Adapted from http://en.wikipedia.org/wiki/Normal_distribution,
+    integration by parts of cumulative distribution function.
 
     Arguments:
-    z -- normal z value
+        z: normal z value
+
+    Returns:
+        Cumulative probability from -∞ to z.
     """
-    Z_MAX = 6.0
-    if z == 0.0:
-        x = 0.0
-    else:
-        y = 0.5 * math.fabs(z)
-        if y >= Z_MAX * 0.5:
-            x = 1.0
-        elif y < 1.0:
-            w = y * y
-            x = ((((((((0.000124818987 * w
-                        - 0.001075204047) * w + 0.005198775019) * w
-                      - 0.019198292004) * w + 0.059054035642) * w
-                    - 0.151968751364) * w + 0.319152932694) * w
-                  - 0.531923007300) * w + 0.797884560593) * y * 2.0
-        else:
-            y -= 2.0
-            x = (((((((((((((-0.000045255659 * y
-                             + 0.000152529290) * y - 0.000019538132) * y
-                           - 0.000676904986) * y + 0.001390604284) * y
-                         - 0.000794620820) * y - 0.002034254874) * y
-                       + 0.006549791214) * y - 0.010557625006) * y
-                     + 0.011630447319) * y - 0.009279453341) * y
-                   + 0.005353579108) * y - 0.002141268741) * y
-                 + 0.000535310849) * y + 0.999936657524
-    if z > 0:
-        return (x + 1.0) * 0.5
-    return (1.0 - x) * 0.5
+    cnt = 40  # number of expansion elements to use.
+    exp = np.array([2*i+1 for i in range(0, cnt+1)])
+    za = np.ones(cnt+1)*z
+    num = np.power(za, exp)
+    denum = np.cumprod(exp)
+    return 0.5+(np.sum(num/denum)/math.sqrt(2*math.pi))*math.exp(-(z*z)/2)
 
 
 def pochisq(x, df=255):
-    """Compute probability of χ² test value. Adapted from: Hill,
-    I. D. and Pike, M. C.  Algorithm 299 Collected Algorithms for the
-    CACM 1967 p. 243 Updated for rounding errors based on remark in
-    ACM TOMS June 1985, page 185.
+    """
+    Compute probability of χ² test value. Adapted from: Hill, I. D. and Pike,
+    M. C.  Algorithm 299 Collected Algorithms for the CACM 1967 p. 243 Updated
+    for rounding errors based on remark in ACM TOMS June 1985, page 185.
 
     According to http://www.fourmilab.ch/random/:
 
-      We interpret the percentage (return value*100) as the degree to
-      which the sequence tested is suspected of being non-random. If
-      the percentage is greater than 99% or less than 1%, the sequence
-      is almost certainly not random. If the percentage is between 99%
-      and 95% or between 1% and 5%, the sequence is
-      suspect. Percentages between 90% and 95% and 5% and 10% indicate
-      the sequence is “almost suspect”.
+      We interpret the percentage (return value*100) as the degree to which
+      the sequence tested is suspected of being non-random. If the percentage
+      is greater than 99% or less than 1%, the sequence is almost certainly
+      not random. If the percentage is between 99% and 95% or between 1% and
+      5%, the sequence is suspect. Percentages between 90% and 95% and 5% and
+      10% indicate the sequence is “almost suspect”.
 
     Arguments:
-    x -- obtained chi-square value
-    df -- degrees of freedom, defaults to 255 for random bytes.
+        x: Obtained chi-square value.
+        df: Degrees of freedom, defaults to 255 for random bytes.
+
+    Returns:
+        The degree to which the sequence tested is suspected of being
+        non-random.
     """
     # Check arguments first
     if not isinstance(df, int):
@@ -149,7 +200,7 @@ def pochisq(x, df=255):
 
     if df > 1:
         y = ex(-a)
-    s = even(y, 2.0 * _poz(-math.sqrt(x)))
+    s = even(y, 2.0 * poz(-math.sqrt(x)))
     if df > 2:
         x = 0.5 * (df - 1.0)
         z = even(1.0, 0.5)
@@ -171,40 +222,6 @@ def pochisq(x, df=255):
             return c * y + s
     else:
         return s
-
-
-def main(args=None):
-    """Calculates and prints figures about the randomness of the input
-    files. Use test data if no arguments given
-
-    Arguments:
-    args -- list of input files
-    """
-    if not args:
-        args = ['test/random.dat']
-    for fname in args:
-        data, cnts = readdata(fname)
-        print('File "{}"'.format(fname))
-        e = entropy(cnts)
-        print('- Entropy is {:.6f} bits per byte.'.format(e))
-        outs = '- Arithmetic mean value of data bytes is {:.4f}'
-        print(outs.format(data.mean()), '(random = 127.5).')
-        c = pearsonchisquare(data, cnts)
-        p = pochisq(c)
-        outs = '- χ² distribution for {} samples is {:.2f}, and randomly'
-        print(outs.format(len(data), c))
-        outs = '  would exceed this value {:.2f} percent of the times.'
-        print(outs.format(p*100))
-        d = math.fabs(p*100-50)
-        print("- According to the χ² test, this sequence", end=' ')
-        if d > 49:
-            print("is almost certainly not random")
-        elif d > 45:
-            print("is suspected of being not random.")
-        elif d > 40:
-            print("is close to random, but not perfect.")
-        else:
-            print("looks random.")
 
 
 if __name__ == '__main__':
